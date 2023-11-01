080313ffd9be647459e15a24e2e9f52c230714e1
theriddler <riddle.hunter@gmail.com>
2023-11-01 03:51:40 -0400

    refactor `GuessTableRow`

--- src/components/GuessTable.tsx
+++ src/components/GuessTable.tsx
@@ -12,39 +12,47 @@ export class GuessTable extends React.Component<GuessTableProps,{}>{
       <table className='guess-table'>
         <tbody>
           {
+            this.props.guesses.length > 0 && 
+              <GuessTableRow fullBorder={true} guess={this.props.guesses[this.props.guesses.length-1]} currentDefinition={this.props.currentDefinition || []}/> 
+          }
+          {
             this.props.guesses
+            .slice(0, this.props.guesses.length-1)
             .sort((g1, g2) => g2.similarity - g1.similarity)
-            .map(guess => {
-              return (
-                <tr>
-                  <td>
-                    {
-                      guess.value
-                      .map((word: string, idx: number) => (
-                        <>
-                          <span 
-                            className='d-inline-block guessed-word-display'
-                            style={{backgroundColor: 
-                              this.props.currentDefinition?.[idx] === word
-                                ? 'green'
-                                : this.props.currentDefinition?.some((w: Lowercase<string>) => w === word) 
-                                  ? 'orange'
-                                  : 'red'
-                          }}>
-                            {word}
-                          </span>
-                          &nbsp;
-                        </>
-                      ))
-                    }
-                  </td>
-                  <td>{guess.similarity}%</td>
-                </tr>
-              )
-            })
+            .map(guess => <GuessTableRow fullBorder={false} guess={guess} currentDefinition={this.props.currentDefinition || []}/>)
           }
         </tbody>
       </table>
     )
   }
 }
+
+const GuessTableRow = (props: {guess: Guess, currentDefinition: Lowercase<string>[], fullBorder: boolean}) => (
+  <tr style={{borderBottom: props.fullBorder ? '1px solid purple' : ''}}>
+    <td className='p-3'>
+      {
+        props.guess.value
+        .map((word: string, idx: number) => (
+          <>
+            <span 
+              key={word}
+              className='d-inline-block guessed-word-display'
+              style={{backgroundColor: 
+                props.currentDefinition?.[idx] === word
+                  ? 'green'
+                  : props.currentDefinition?.some((w: Lowercase<string>) => w === word) 
+                    ? 'orange'
+                    : 'red'
+            }}>
+              {word}
+            </span>
+            &nbsp;
+          </>
+        ))
+      }
+    </td>
+    <td className='p-3'>
+      {props.guess.similarity}%
+    </td>
+  </tr>
+)
