4e7b787eea82a4f6a6b851391b3f216a93a23b5b
theriddler <riddle.hunter@gmail.com>
2023-11-01 13:16:12 -0400

    GuessTable is prettier

--- src/components/GuessTable.tsx
+++ src/components/GuessTable.tsx
@@ -3,63 +3,60 @@ import { Guess } from "../types";
 
 interface GuessTableProps {
   guesses: Guess[];
+  previousGuess?: Guess;
   currentDefinition?: Lowercase<string>[];
 }
 
 interface GuessTableState {
-  lastGuess?: Guess;
 }
 
 export class GuessTable extends React.Component<GuessTableProps,GuessTableState>{
   constructor(p: GuessTableProps){
     super(p);
-
-    this.state = {
-      lastGuess: undefined
-    }
   }
-  render() {
-    this.setState({lastGuess:this.props.guesses[-1]})
 
+  render() {
     return (
-      <table className='guess-table'>
-        <tbody>
-          {
-            this.props.guesses
-            .sort((g1, g2) => g2.similarity - g1.similarity)
-            .map(guess => <GuessTableRow lastGuess={guess.value.every((w,idx) => w === guess.value[idx])} guess={guess} currentDefinition={this.props.currentDefinition || []}/>)
-          }
-        </tbody>
-      </table>
+      <div className='guess-table'>
+        {
+          this.props.guesses
+          .sort((g1, g2) => g2.similarity - g1.similarity)
+          .map(guess => <GuessTableRow key={guess.value.join('')} previousGuess={guess.value.every((w,idx) => w === this.props.previousGuess?.value[idx])} guess={guess} currentDefinition={this.props.currentDefinition || []}/>)
+        }
+      </div>
     )
   }
 }
 
-const GuessTableRow = (props: {guess: Guess, currentDefinition: Lowercase<string>[], lastGuess: boolean}) => (
-  <tr>
-    <td className='p-3'>
+const GuessTableRow = (props: {guess: Guess, currentDefinition: Lowercase<string>[], previousGuess: boolean}) => (
+  <div className='guess-row'>
+    <div className='p-2'>
       {
         props.guess.value
         .map((word: string, idx: number) => (
-          <>
-            <span 
-              key={word}
-              className='d-inline-block guessed-word-display'
-              style={{color: 
-                props.currentDefinition?.[idx] === word
-                  ? 'green'
-                  : props.currentDefinition?.some((w: Lowercase<string>) => w === word) 
-                    ? 'darkorange'
-                    : 'red'
-            }}>
-              {word}
-            </span>
-          </>
+          <span 
+            key={`${word}_${idx}`}
+            className='d-inline-block guessed-word-display'
+            style={{color: 
+              props.currentDefinition?.[idx] === word
+                ? 'green'
+                : props.currentDefinition?.some((w: Lowercase<string>) => w === word) 
+                  ? 'darkorange'
+                  : 'red'
+          }}>
+            {word}
+          </span>
         ))
       }
-    </td>
-    <td className='p-3' style={{color: props.lastGuess ? 'green' : 'white'}}>
-      {props.guess.similarity}%
-    </td>
-  </tr>
+    </div>
+    <div className='p-2'>
+      <span  
+        key={`${props.guess.value.join('')}_${props.guess.similarity}`}
+        className='px-3 d-inline-block guessed-word-display'
+        style={{border: props.previousGuess ? '1px solid white' : '0'}}
+      >
+        {props.guess.similarity}%
+      </span>
+    </div>
+  </div>
 )
